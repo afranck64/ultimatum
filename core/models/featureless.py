88ba88e88beb418c75e86bafa105618094cc6a7b
afranck64 <afranck64@yahoo.fr>
2019-07-30 23:08:55 +0200

    change distribution for the random model

--- core/models/featureless.py
+++ core/models/featureless.py
@@ -1,5 +1,8 @@
 import numpy as np
+import pandas as pd
+
 from .metrics import avg_loss_ratio, MAX_GAIN
+from ..utils import randn_skew_fast
 
 class EMModel(object):
     def __init__(self, max_value=MAX_GAIN, loss=None):
@@ -30,14 +33,21 @@ class RandomModel(object):
     def __init__(self, max_value=MAX_GAIN):
         self.value = max_value
         self._trained = False
+        self.mean = 0
+        self.std = 0
+        self.skew = 0
 
     def fit(self, xTrain, yTrain, **kwargs):
+        self.mean = np.mean(yTrain)
+        self.std = np.std(yTrain)
+        self.skew = pd.DataFrame(data=xTrain).skew()[0]
         self._trained = True
     
     def predict(self, xTest, **kwargs):
         if not self._trained:
             raise ValueError("The model should first be trained")
-        return np.random.random((xTest.shape[0], 1)) * self.value
+        res = randn_skew_fast(xTest.shape[0], self.skew, self.mean, self.std)
+        return res
 
 
 class ConservativeModel(object):
