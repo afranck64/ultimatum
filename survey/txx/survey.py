e4d5d87427991b66b000ad8bcad1ec333ea4f7b0
afranck64 <afranck64@yahoo.fr>
2019-09-06 16:40:50 +0200

    connect adapter and submission

--- survey/txx/survey.py
+++ survey/txx/survey.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 import requests
@@ -6,12 +7,12 @@ from flask_wtf.form import FlaskForm
 from wtforms.validators import DataRequired
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, BooleanField, RadioField, TextAreaField
 
-from survey.utils import get_table, save_result2db, save_result2file, get_output_filename, get_latest_treatment
+from survey.utils import get_table, save_result2db, save_result2file, get_output_filename, get_latest_treatment, generate_completion_code, save_worker_id
 from survey.db import insert, get_db
 from survey._app import app
-from survey.adapter import get_adapter
+from survey.adapter import get_adapter, get_adapter_from_dict
 
-BASE = "home"
+BASE = os.path.splitext(os.path.split(__file__)[1])[0]
 
 class MainForm(FlaskForm):
     gender = RadioField("What is your gender?", choices=[
@@ -83,7 +84,8 @@ def handle_survey(treatment=None, template=None):
     session["job_id"] = job_id
     session["worker_id"] = worker_id
     session["treatment"] = treatment
-    session["txx"] = True
+    session["adapter"] = adapter.to_dict()
+    session[BASE] = True
     form = MainForm(request.form)
     if request.method == "POST" and form.validate_on_submit():
         form = MainForm(request.form)
@@ -91,23 +93,42 @@ def handle_survey(treatment=None, template=None):
         return redirect(url_for("survey_done"))
     return render_template(template, job_id=job_id, worker_id=worker_id, treatment=treatment, form=form)
 
-def handle_survey_done():
-    if not session.get("txx"):
+def handle_survey_done(template=None):
+    if template is None:
+        template = "txx/info.html"
+    if not session.get(BASE):
         flash("Sorry, you are not allowed to use this service. ^_^")
         return render_template("error.html")
-    job_id = session["job_id"]
-    worker_id = session["worker_id"]
-    treatment = session["treatment"]
+    worker_code_key = f"{BASE}__worker_code"
+    if not session.get(worker_code_key):
+        job_id = session["job_id"]
+        worker_id = session["worker_id"]
+        treatment = session["treatment"]
 
-    response = session["response"]
-    response["worker_id"] = worker_id
-    result = {k:v for k,v in response.items() if k not in {'csrf_token'}}
-    try:
-        save_result2file(get_output_filename(base="survey", job_id=job_id, treatment=treatment), result)
-    except Exception as err:
-        app.log_exception(err)
-    try:
-        save_result2db(table=get_table(base="survey", job_id=job_id, schema="result", treatment=treatment), response_result=result, unique_fields=["worker_id"])
-    except Exception as err:
-        app.log_exception(err)
-    return render_template("info.html", job_id=job_id)
+        worker_code = generate_completion_code(base=treatment, job_id=job_id)
+        response = session["response"]
+        response["worker_id"] = worker_id
+        result = {k:v for k,v in response.items() if k not in {'csrf_token'}}
+        try:
+            save_result2file(get_output_filename(base=BASE, job_id=job_id, treatment=treatment), result)
+        except Exception as err:
+            app.log_exception(err)
+        try:
+            save_result2db(table=get_table(base=BASE, job_id=job_id, schema="result", treatment=treatment), response_result=result, unique_fields=["worker_id"])
+        except Exception as err:
+            app.log_exception(err)
+
+        adapter = get_adapter_from_dict(session["adapter"])
+        app.logger.debug(f"adapter: {adapter} - {session['adapter']}")
+        session.clear()
+        session[BASE] = True
+        session[worker_code_key] = worker_code
+
+        submit_to_URL = adapter.get_submit_to_URL()
+        if submit_to_URL is not None:
+            try:
+                requests.post(submit_to_URL, json=adapter.get_submit_to_kwargs())
+            except Exception as err:
+                app.logger.warn(f"{err}")
+        # save_worker_id(job_id=job_id, worker_id=worker_id)
+    return render_template(template, worker_code=session[worker_code_key])
