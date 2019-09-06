4f918e00e107a03f16e1d99a1039ce04653efe35
afranck64 <afranck64@yahoo.fr>
2019-09-06 16:38:17 +0200

    add dict transformations in adapter

--- survey/adapter.py
+++ survey/adapter.py
@@ -18,16 +18,28 @@ class BaseAdapter(object):
     def get_submit_to_URL(self):
         raise NotImplementedError
     
+    def get_submit_to_kwargs(self, **kwargs):
+        raise NotImplementedError
+    
     def is_preview(self):
-        return NotImplementedError
+        raise NotImplementedError
+    
+    def to_dict(self):
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(self, dict_obj):
+        raise NotADirectoryError
+
 
 class DefaultAdapter(object):
     def __init__(self):
-        self.job_id = request.args.get("job_id")
-        self.worker_id = request.args.get("worker_id")
-        self.unit_id = request.args.get("unit_id")
+        self.job_id = request.args.get("job_id", "na")
+        self.worker_id = request.args.get("worker_id", "na")
+        self.unit_id = request.args.get("unit_id", "na")
         self.submit_to_URL = request.args.get("submit_to_URL")
         self.preview = request.args.get("preview") in {"1", "true"}
+        self.submit_to_kwargs = {}
 
     def get_job_id(self):
         return self.job_id
@@ -41,8 +53,24 @@ class DefaultAdapter(object):
     def get_submit_to_URL(self):
         return self.submit_to_URL
     
+    def get_submit_to_kwargs(self):
+        return self.submit_to_kwargs
+
     def is_preview(self):
         return self.is_preview
+    
+    def to_dict(self):
+        obj_dict = dict(self.__dict__)
+        obj_dict["_adapter"] = None
+        return obj_dict
+    
+    @classmethod
+    def from_dict(cls, dict_obj):
+        adapter_key = dict_obj.get("_adapter")
+        adapter_cls = ADAPTERS[adapter_key]
+        adapter = adapter_cls()
+        adapter.__dict__.update(dict_obj)
+        return adapter
 
 class MTurkAdapter(DefaultAdapter):
     def __init__(self):
@@ -51,6 +79,14 @@ class MTurkAdapter(DefaultAdapter):
         self.unit_id = request.args.get("assignmentId")
         self.submit_to_URL = request.args.get("turkSubmitTo")
         self.preview = request.args.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE"
+        self.submit_to_kwargs = {
+            "assignmentId": request.args.get("assignmentId")
+        }
+
+    def to_dict(self):
+        obj_dict = dict(self.__dict__)
+        obj_dict["_adapter"] = None
+        return obj_dict
 
 ADAPTERS = defaultdict(
     lambda: DefaultAdapter,
@@ -64,3 +100,11 @@ def get_adapter():
     adapter_cls = ADAPTERS[adapter_key]
     app.logger.debug(f"get_adapter: {adapter_cls.__name__}")
     return adapter_cls()
+
+def get_adapter_from_dict(dict_obj):
+    adapter_key = dict_obj.get("_adapter")
+    adapter_cls = ADAPTERS[adapter_key]
+    adapter = adapter_cls()
+    adapter.__dict__.update(dict_obj)
+    return adapter
+    
