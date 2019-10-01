25d732a021ce14d5e9d7a547a72ab25798227cdf
afranck64 <afranck64@yahoo.fr>
2019-10-02 00:42:23 +0200

    clean and redesign

--- survey/tasks/risk.py
+++ survey/tasks/risk.py
@@ -1,45 +1,65 @@
 import os
 import datetime
-import time
-
+import random
 from flask import (
-    Blueprint, render_template, request, redirect, flash, url_for, make_response, g
+    Blueprint, render_template
 )
 
 from survey._app import csrf_protect
-from survey.utils import set_cookie_obj, get_cookie_obj
-
 from survey.tasks.task import handle_task_done, handle_task_index
 
 
 #### const
-bp = Blueprint("tasks.risk", __name__)
-
 BASE = os.path.splitext(os.path.split(__file__)[1])[0]
-BASE = "risk"
-CELL_BONUS_CENTS = 2
-NB_CELLS = 50
-MAX_BONUS = NB_CELLS * CELL_BONUS_CENTS
+bp = Blueprint(f"tasks.{BASE}", __name__)
 
-FIELDS = {f"cell{i}" for i in range(1, 51)}
+FIELDS = {"q1", "q2", "q3", "q4"}
 def validate_response(response):
     for field in FIELDS:
         if field not in response:
             return False
     return True
 
+INDEX_PROBABILITY = 0
+INDEX_GAIN = 1
+CHOICES = {
+    "q1": [(1,1), (0.9, 1.1), (0.8, 1.3), (0.7, 1.5), (0.6, 1.7), (0.5, 2.1), (0.4, 2.7), (0.3, 3.6), (0.2, 5.4), (0.1, 10.90)],
+    "q2": [(1,1), (0.9, 1.2), (0.8, 1.5), (0.7, 1.9), (0.6, 2.3), (0.5, 3.0), (0.4, 4.0), (0.3, 5.7), (0.2, 9.0), (0.1, 19.90)],
+    "q3": [(1,1), (0.9, 1.7), (0.8, 2.5), (0.7, 3.6), (0.6, 5.0), (0.5, 7.0), (0.4, 10), (0.3, 15), (0.2, 25), (0.1, 55)],
+    "q4": [(1,1), (0.9, 2.2), (0.8, 3.8), (0.7, 5.7), (0.6, 5.0), (0.5, 7.0), (0.4, 10), (0.3, 15), (0.2, 25), (0.1, 100)],
+}
+
+MAX_BONUS = sum(options[-1][1] for options in CHOICES.values()) / len(CHOICES)
+
+FEATURES = set(f"{BASE}_{k}" for k in FIELDS) | set({
+    f"{BASE}_expectated_value",
+    f"{BASE}_time_spent"
+})
+
 def response_to_bonus(response):
     bonus = 0
-    for field in FIELDS:
-        bonus += response[field] * CELL_BONUS_CENTS
+    tmp_bonus = 0.0
+    for key, options in CHOICES.items():
+        user_choice = int(response[key])
+        user_p = options[user_choice][INDEX_PROBABILITY]
+        p = random.random()
+        if user_p > p:
+            tmp_bonus += options[user_choice][INDEX_GAIN]
+    bonus = round(tmp_bonus / len(CHOICES))
     return bonus
+
+def get_expected_value_from_response(response):
+    tmp_expected_value = 0
+    for key, options in CHOICES.items():
+        user_choice = int(response[key])
+        tmp_expected_value += options[user_choice][INDEX_GAIN] * options[user_choice][INDEX_PROBABILITY]
+    expected_value = round(tmp_expected_value / len(CHOICES), 4)
+    return expected_value
 
 
 def response_to_result(response, job_id=None, worker_id=None):
     """
     :returns: {
-        cells: number of opened cells
-        time_spent_risk: time (ms) spent on this task
         timestamp: server time when genererting the result
         job_id: fig-8 job id
         worker_id: fig-8 worker id
@@ -47,9 +67,10 @@ def response_to_result(response, job_id=None, worker_id=None):
     }
     """
     result = dict(response)
-    result["cells"] = response_to_bonus(response)//CELL_BONUS_CENTS
-    result["time_spent_risk"] = int(response["time_stop"] - response["time_start"]) * 1000
+    result.update({f"{BASE}_{key}": CHOICES[key][response[key]][INDEX_PROBABILITY]*CHOICES[key][response[key]][INDEX_GAIN] for key in FIELDS})
     result["timestamp"] = str(datetime.datetime.now())
+    result["risk_expectated_value"] = get_expected_value_from_response(response)
+    result["risk_time_spent"] = result["time_spent"]
     result["job_id"] = job_id
     result["worker_id"] = worker_id
     result["worker_bonus"] = response_to_bonus(response)
@@ -57,59 +78,11 @@ def response_to_result(response, job_id=None, worker_id=None):
 ############
 
 @csrf_protect.exempt
-@bp.route("/risk/", methods=["GET", "POST"])
+@bp.route(f"/{BASE}/", methods=["GET", "POST"])
 def index():
-    #return handle_task_index("risk", validate_response=validate_response)
-
-    cookie_obj = get_cookie_obj(BASE)
-    worker_code_key = f"{BASE}_worker_code"
-    worker_id = request.args.get("worker_id", "na")
-    job_id = request.args.get("job_id", "na")
-    # The task was already completed, so we skip to the completion code display
-    if cookie_obj.get(BASE) and cookie_obj.get(worker_code_key) and cookie_obj.get("worker_id") == worker_id:
-        req_response =  make_response(redirect(url_for(f"tasks.{BASE}.done")))
-        return req_response
-
-    cookie_obj = get_cookie_obj(BASE)
-    g.cookie_obj = cookie_obj
-    if request.method == "GET":
-        cookie_obj[BASE] = True
-        cookie_obj["worker_id"] = worker_id
-        cookie_obj["job_id"] = job_id
-        cookie_obj["time_start"] = time.time()
-        cookie_obj["cells"] = {f"cell{cid}":0 for cid in range(1, 51)}
-    if request.method == "POST":
-        #response = request.form.to_dict()
-        response = cookie_obj["cells"]
-        if validate_response is not None and validate_response(response):
-            response["time_stop"] = time.time()
-            response["time_start"] = cookie_obj.get("time_start")
-            cookie_obj["response"] = response
-            req_response = make_response(redirect(url_for(f"tasks.{BASE}.done")))
-            set_cookie_obj(req_response, BASE, cookie_obj)
-            return req_response
-
-    req_response = make_response(render_template(f"tasks/{BASE}.html"))
-    cookie_obj[BASE] = True
-    set_cookie_obj(req_response, BASE, cookie_obj)
-    return req_response
+    return handle_task_index(f"{BASE}", validate_response=validate_response, template_kwargs={"options": CHOICES})
 
 
-
-@bp.route("/risk/done/")
+@bp.route(f"/{BASE}/done")
 def done():
-    return handle_task_done("risk", unique_fields=["worker_id"], response_to_result_func=response_to_result, response_to_bonus=response_to_bonus)
-
-@bp.route("/risk/check/")
-def check():
-    cookie_obj = get_cookie_obj(BASE)
-    if not cookie_obj.get(BASE, None):
-        flash("Sorry, you are not allowed to use this service. ^_^")
-        return render_template("error.html")
-    cell = request.args.get("cell")
-    cells = cookie_obj["cells"]
-    cells[cell] = 1
-    cookie_obj["cells"] = cells
-    req_response = make_response("")
-    set_cookie_obj(req_response, BASE, cookie_obj)
-    return req_response
+    return handle_task_done(f"{BASE}", numeric_fields="*", unique_fields=["worker_id"], response_to_result_func=response_to_result, response_to_bonus=response_to_bonus)
