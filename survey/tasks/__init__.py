0861137e555877bd2eeefdf51a8ce0d651f6d38f
afranck64 <afranck64@yahoo.fr>
2019-10-07 20:05:51 +0200

    compute max-gain base of tasks listed in config["TASKS"]

--- survey/tasks/__init__.py
+++ survey/tasks/__init__.py
@@ -4,7 +4,27 @@ from survey._app import app
 bp = Blueprint("tasks", __name__)
 
 # MAX_BONUS = cg.MAX_BONUS + crt.MAX_BONUS + eff.MAX_BONUS + hexaco.MAX_BONUS + risk.MAX_BONUS
-MAX_BONUS = 100
+MAX_BONUS = 0
+if "cc" in app.config["TASKS"]:
+    MAX_BONUS += cc.MAX_BONUS
+if "cg" in app.config["TASKS"]:
+    MAX_BONUS += cg.MAX_BONUS
+if "cpc" in app.config["TASKS"]:
+    MAX_BONUS += cpc.MAX_BONUS
+if "crt" in app.config["TASKS"]:
+    MAX_BONUS += crt.MAX_BONUS
+if "eff" in app.config["TASKS"]:
+     MAX_BONUS += eff.MAX_BONUS
+if "goat" in app.config["TASKS"]:
+     MAX_BONUS += goat.MAX_BONUS
+if "hexaco" in app.config["TASKS"]:
+     MAX_BONUS += hexaco.MAX_BONUS
+if "risk" in app.config["TASKS"]:
+     MAX_BONUS += risk.MAX_BONUS
+if "exp" in app.config["TASKS"]:
+     MAX_BONUS += exp.MAX_BONUS
+if "ras" in app.config["TASKS"]:
+     MAX_BONUS += ras.MAX_BONUS
 
 TASKS_FEATURES = {
     "cg": cg.FEATURES,
