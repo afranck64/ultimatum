022db13ecedf4ecb3c42fd2b196714ceb6bbf812
afranck64 <afranck64@yahoo.fr>
2019-10-14 13:32:43 +0200

    improve logging

--- survey/_app.py
+++ survey/_app.py
@@ -53,9 +53,49 @@ def _env2bool(env_value):
         return False
     return env_value.upper() in {"YES", "TRUE", "ENABLED"}
 
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
 app.config["DEBUG"] = _env2bool(os.getenv("DEBUG"))
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+if app.config["DEBUG"]:
+    app.logger.setLevel(logging.DEBUG)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
 app.config["MTURK_SANDBOX"] = _env2bool(os.getenv("MTURK_SANDBOX"))
-app.logger.warn(f"MTURK_SANDBOX: {app.config['MTURK_SANDBOX']}")
+app.logger.info(f"MTURK_SANDBOX: {app.config['MTURK_SANDBOX']}, AWS_PROFILE: {os.getenv('AWS_PROFILE')}")
 app.config["TASKS"] = ["exp", "risk", "cc", "ras", "cpc"]
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", os.urandom(32))
 app.config["APPLICATION_ROOT"] = os.getenv("APPLICATION_ROOT", "/")
@@ -115,44 +155,3 @@ app.config["TREATMENTS_AUTO_DSS"] = TREATMENTS_AUTO_DSS
 app.config["OUTPUT_DIR"] = os.getenv("OUTPUT_DIR", "./data/output")
 os.makedirs(app.config["OUTPUT_DIR"], exist_ok=True)
 
-class ReverseProxied(object):
-    '''Wrap the application in this middleware and configure the 
-    front-end server to add these headers, to let you quietly bind 
-    this to a URL other than / and to an HTTP scheme that is 
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    '''
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        return self.app(environ, start_response)
-
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-app.logger.setLevel(logging.DEBUG)
-app.wsgi_app = ReverseProxied(app.wsgi_app)
-
-from survey.db import init_app, init_db
-
-#init_app(app)
