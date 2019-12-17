a16d72bf7e9729180c265401687eee3366c162ef
afranck64 <afranck64@yahoo.fr>
2019-12-17 16:31:03 +0100

    include demographics-stats generation

--- snippets/generate_treatment_stats.py
+++ snippets/generate_treatment_stats.py
@@ -15,17 +15,81 @@ import argparse
 db = ":memory:"
 
 STATS_FUNCTIONS = {}
+DEMOGRAPHICS_FUNCTIONS = {}
 
-def mark_for_stats(function, label=None):
-    if label is None:
-        label = function.__name__[4:]
-    STATS_FUNCTIONS[label] = function
-    return function
+ETHNICITIES = dict([
+    ("american_indian_or_alaska_native", "American Indian or Alaska Native"),
+    ("asian", "Asian"),
+    ("black_or_african_american", "Black or African American"),
+    ("hispanic_latino", "Hispanic/Latino"),
+    ("hawaiian_or_pacific_islander", "Native Hawaiian or Pacific Islander"),
+    ("white", "White/Caucasian"),
+    ("other", "Other")
+])
+
+AGES = dict([
+    ("1825_years", "18-25 Years"),
+    ("2635_years", "26-35 Years"),
+    ("3645_years", "36-45 Years"),
+    ("4655_years", "46-55 Years"),
+    ("5665_years", "56-65 Years"),
+    ("older_than_65_years", "Older than 65 Years")
+])
+
+EDUCATIONS = dict([
+    ("less_than_high_school_diploma", "Less than a high school diploma"),
+    ("high_school_degree", "High school graduate, diploma or the equivalent"),
+    ("bachelor_degree", "Bachelor's degree"),
+    ("master_degree", "Master's degree"),
+    ("doctorate_degree", "Doctorate degree"),
+    ("other", "Other")
+])
+
+GENDERS = dict([
+    ("female", "Female"),
+    ("male", "Male"),
+    ("other", "Other")
+])
+
+INCOMES = dict([
+    ("Primary source of income", "Primary source of income"),
+    ("Secondary source of income", "Secondary source of income"),
+    ("I earn nearly equal incomes from crowdsourced microtasks and other job(s)", "I earn nearly equal incomes from crowdsourced microtasks and other job(s)")
+])
+
+
+# overwritten by a command line flag. If true, percentage will be generated instead of frequency
+USE_PERCENTAGE = None
+
+USE_LABELS = None
+
+SELECTION = None
+
+def mark_for_stats(label=None):
+    def _mark_for_stats(function, label=label):
+        if label is None:
+            label = function.__name__[4:]
+        STATS_FUNCTIONS[label] = function
+        return function
+    return _mark_for_stats
+
+
+def mark_for_demographics(label=None):
+    def _mark_for_stats(function, label=label):
+        if label is None:
+            label = function.__name__[4:]
+        DEMOGRAPHICS_FUNCTIONS[label] = function
+        return function
+    return _mark_for_stats
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Generate statistics for a given treatment')
-    parser.add_argument('--output_dir', help='Output directory to save csv files')
-    parser.add_argument('treatment', help='treatment to export')
+    parser.add_argument('--use-percentage', help='Generate percentages instead of frequencies', action='store_true')
+    parser.add_argument('--use-latex', help='Print results as latex table', action='store_true')
+    parser.add_argument('--use-labels', help='Print results using description labels', action='store_true')
+    parser.add_argument('--output-dir', help='Output directory where csv files were exported')
+    parser.add_argument('--selection', help='Whether to restrict the stats to responder or proposers', choices=['prop', 'resp'])
+    parser.add_argument('treatments', help='Comma separated treatments')
     return parser
 
 
@@ -52,7 +116,7 @@ def get_tables(treatment):
     return tables
 
 FEEDBACK_COLUMNS = ["feedback_accuracy", "feedback_explanation", "feedback_explanation"]
-def get_con(treatment, db=None):
+def get_con_and_dfs(treatment, db=None):
     if db is None:
         db = ":memory:"
     con = sqlite3.connect(
@@ -60,7 +124,7 @@ def get_con(treatment, db=None):
         detect_types=sqlite3.PARSE_DECLTYPES
     )
     con.row_factory = sqlite3.Row
-
+    dfs = {}
     output_dir = get_output_dir(treatment)
     for table in get_tables(treatment):
         try:
@@ -79,11 +143,13 @@ def get_con(treatment, db=None):
                     for col in FEEDBACK_COLUMNS:
                         df[col] = df_feedback[col]
                 except Exception as err:
+                    pass
             df.to_sql(table, con)
+            dfs[table] = df
         except Exception as err:
             print("err: ", err)
             pass
-    return con
+    return con, dfs
 
 def get_tasks_tables():
     return {
@@ -103,8 +169,8 @@ def get_tasks_jobs_ids(treatment, con):
         jobs_ids = []
     return jobs_ids
 
-@mark_for_stats
-def get_nb_participants(treatment, con):
+@mark_for_stats()
+def get_nb_participants(treatment, con, dfs=None):
     sql = f"""
         select count (distinct(worker_id)) as nb_participants from (
             select worker_id , job_id from result__{treatment}_prop where job_id!="na"
@@ -117,8 +183,8 @@ def get_nb_participants(treatment, con):
     """
     return con.execute(sql).fetchone()[0]
 
-@mark_for_stats
-def get_nb_rejected_participants(treatment, con):
+@mark_for_stats()
+def get_nb_rejected_participants(treatment, con, dfs=None):
     sql = f"""
 
         select count (distinct(worker_id)) as nb_rejected_participants from (
@@ -131,8 +197,8 @@ def get_nb_rejected_participants(treatment, con):
     return con.execute(sql).fetchone()[0]
 
 
-@mark_for_stats
-def get_nb_games_with_demographics(treatment, con):
+@mark_for_stats()
+def get_nb_games_with_demographics(treatment, con, dfs=None):
     sql = f"""
         select count(*) as nb_games_with_demographics from (
             select resp_worker_id, prop_worker_id, job_id from result__{treatment}_prop where job_id!="na"
@@ -144,8 +210,8 @@ def get_nb_games_with_demographics(treatment, con):
     """
     return con.execute(sql).fetchone()[0]
 
-@mark_for_stats
-def get_nb_responders(treatment, con):
+@mark_for_stats()
+def get_nb_responders(treatment, con, dfs=None):
     sql = f"""
         select count (distinct(worker_id)) as nb_responders from (
             select worker_id , job_id from result__{treatment}_resp where job_id!="na"
@@ -154,8 +220,8 @@ def get_nb_responders(treatment, con):
     """
     return con.execute(sql).fetchone()[0]
 
-@mark_for_stats
-def get_nb_responders_with_demographics(treatment, con):
+@mark_for_stats()
+def get_nb_responders_with_demographics(treatment, con, dfs=None):
     sql = f"""
         select count (distinct(worker_id)) as nb_responders_with_demographics from (
             select worker_id ,job_id from result__{treatment}_resp where job_id!="na"
@@ -167,8 +233,8 @@ def get_nb_responders_with_demographics(treatment, con):
     return con.execute(sql).fetchone()[0]
 
 
-@mark_for_stats
-def get_nb_proposers(treatment, con):
+@mark_for_stats()
+def get_nb_proposers(treatment, con, dfs=None):
     sql = f"""
         select count (distinct(worker_id)) as nb_proposers from (
             select worker_id , job_id from result__{treatment}_prop where job_id!="na"
@@ -178,8 +244,8 @@ def get_nb_proposers(treatment, con):
     return con.execute(sql).fetchone()[0]
 
 
-@mark_for_stats
-def get_nb_proposers_with_demographics(treatment, con):
+@mark_for_stats()
+def get_nb_proposers_with_demographics(treatment, con, dfs=None):
     sql = f"""
         select count (distinct(worker_id)) as nb_proposers_with_demographics from (
             select worker_id ,job_id from result__{treatment}_prop where job_id!="na"
@@ -190,22 +256,167 @@ def get_nb_proposers_with_demographics(treatment, con):
     """
     return con.execute(sql).fetchone()[0]
 
+@mark_for_stats()
+def get_nb_males(treatment, con, dfs=None):
+    sql = f"""
+        select count (*) as nb_males
+        from result__{treatment}_survey
+        where gender='male'
+    """
+    return con.execute(sql).fetchone()[0]
 
-def generate_stats(treatment, con):
-    stats = {label:function(treatment, con) for label, function in STATS_FUNCTIONS.items()}
+@mark_for_stats()
+def get_nb_females(treatment, con, dfs=None):
+    sql = f"""
+        select count (*) as nb_females
+        from result__{treatment}_survey
+        where gender='female'
+    """
+    return con.execute(sql).fetchone()[0]
+
+@mark_for_stats()
+def get_nb_gender_other(treatment, con, dfs=None):
+    sql = f"""
+        select count (*) as nb_gender_other
+        from result__{treatment}_survey
+        where gender='other'
+    """
+    return con.execute(sql).fetchone()[0]
+
+@mark_for_demographics()
+def get_ethnicity(treatment, con, dfs=None, use_percentage=None, use_labels=None):
+    sql = f"""
+        select * from result__{treatment}_survey
+        where worker_id in (
+            select worker_id from main__txx where worker_code != 'dropped'
+        )
+    """
+    if use_percentage is None:
+        use_percentage = USE_PERCENTAGE
+    if use_labels is None:
+        use_labels = USE_LABELS
+    labels = ETHNICITIES
+    df = pd.read_sql(sql, con)
+    df = df['ethnicity'].apply(lambda x: tuple(x.split(':')))
+    result = {}
+    for items in df:
+        for item in items:
+            result[item] = result.get(item, 0) + 1/len(items)
+    
+    if use_percentage:
+        result = {k:(result[k] if k in result else 0) for k in labels}
+        result = {k: f"{round(result[k])} ({round(100 * result[k]/len(df), 2)})" for k in result}
+    else:
+        result = {k:(round(result[k]) if k in result else 0) for k in labels}
+
+    if use_labels:
+        label_result = {}
+        for k, l in labels.items():
+            label_result[l] = result.get(k, 0)
+        result = label_result
+    return result
+     
+def _get_demographic_stat(name, labels, treatment, con, dfs=None, use_percentage=None, use_labels=None):
+    sql = f"""
+        select * from result__{treatment}_survey
+        where
+            worker_id in (
+                select worker_id from main__txx where worker_code != 'dropped'
+            )
+    """
+    if SELECTION == "resp":
+        sql += f"""and
+            worker_id in (
+                select worker_id from result__{treatment}_resp
+            )
+        """
+    elif SELECTION == "prop":
+        sql += f"""and
+            worker_id in (
+                select worker_id from result__{treatment}_prop
+            )
+        """
+    if use_percentage is None:
+        use_percentage = USE_PERCENTAGE
+    if use_labels is None:
+        use_labels = USE_LABELS
+    df = pd.read_sql(sql, con)
+    df = df[name].values
+    result = {}
+    for item in df:
+        result[item] = result.get(item, 0) + 1
+
+    result = {k:(result[k] if k in result else 0) for k in labels}
+    if use_percentage:
+        result = {k: f"{round(result[k])} ({round(100 * result[k]/len(df), 2)})" for k in result}
+
+    if use_labels:
+        label_result = {}
+        for k, l in labels.items():
+            label_result[l] = result[k]
+        result = label_result
+    return result
+
+@mark_for_demographics()
+def get_age(treatment, con, dfs=None, use_percentage=None, use_labels=None):
+    return _get_demographic_stat("age", AGES, treatment, con, dfs, use_percentage, use_labels)
+
+@mark_for_demographics()
+def get_eductation(treatment, con, dfs=None, use_percentage=None, use_labels=None):
+    return _get_demographic_stat("education", EDUCATIONS, treatment, con, dfs, use_percentage, use_labels)
+
+
+@mark_for_demographics()
+def get_gender(treatment, con, dfs=None, use_percentage=None, use_labels=None):
+    return _get_demographic_stat("gender", GENDERS, treatment, con, dfs, use_percentage, use_labels)
+
+@mark_for_demographics()
+def get_income(treatment, con, dfs=None, use_percentage=None, use_labels=None):
+    return _get_demographic_stat("income", INCOMES, treatment, con, dfs, use_percentage, use_labels)
+
+
+def generate_stats(treatment, con, dfs):
+    stats = {label:function(treatment, con, dfs) for label, function in STATS_FUNCTIONS.items()}
     return pd.Series(stats)
 
 
+def generate_demographics(treatment, con, dfs):
+    demographics = {label:function(treatment, con, dfs) for label, function in DEMOGRAPHICS_FUNCTIONS.items()}
+    return demographics
+
+
 def main():
+    global USE_PERCENTAGE, USE_LABELS, SELECTION
     parser = get_parser()
     args = parser.parse_args()
-    
-    treatment = args.treatment
 
-    con = get_con(treatment)
+    treatments = args.treatments.split(",")
 
-    stats = generate_stats(treatment, con)
-    print(stats)
+    USE_PERCENTAGE = args.use_percentage
+
+    USE_LABELS = args.use_labels
+
+    SELECTION = args.selection
+
+    for treatment in treatments:
+        con, dfs = get_con_and_dfs(treatment)
+        stats = generate_stats(treatment, con, dfs)
+        demographics = generate_demographics(treatment, con, dfs)
+        print(f"Treatment: {treatment}")
+        if args.use_latex:
+            print(stats.to_latex())
+        else:
+            print(stats)
+        print("\n\n")
+        for k, v in demographics.items():
+            serie = pd.Series(v, name=k)
+            if args.use_latex:
+                print(serie.to_latex())
+            else:
+                print(serie)
+            print()
+        print()
+        print()
 
 
 if __name__ == "__main__":
