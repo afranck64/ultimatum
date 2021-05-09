b5e1db766189ef0f5308aaf3b9303fb761353b24
afranck64 <afranck64@yahoo.fr>
2021-05-10 00:21:16 +0200

    minor cleanup

--- survey/dasbhoard.py
+++ survey/dasbhoard.py
@@ -1,95 +1,22 @@
-import json
-import os
-import warnings
-import random
-import string
-import csv
-import time
-import datetime
-import io
-
-import pandas as pd
-
 from flask import (
-    Blueprint, flash, Flask, g, redirect, render_template, request, url_for, jsonify, Response
+    Blueprint, render_template
 )
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, BooleanField
-from wtforms.validators import DataRequired, NumberRange, InputRequired
-from wtforms.widgets import html5
-from flask_wtf.csrf import CSRFProtect
-from werkzeug.utils import secure_filename
 
 from survey._app import app, csrf_protect
-from survey.db import get_db, table_exists, insert, update
+from survey.db import get_db, table_exists
 
 
-bp = Blueprint("dashboard", __name__)
+bp = Blueprint(__name__, __name__)
 
 
 ###### helpers #####
-class JobConfig(dict):
-    def __init__(self, job_id, api_key, nb_rows=0, unique_worker=True, base_code="", expected_judgments=0, payment_max_cents=0):
-        super().__init__()
-        self["job_id"] = job_id
-        self["api_key"] = api_key
-        self["nb_rows"] = nb_rows
-        self["unique_worker"] = unique_worker
-        self["base_code"] = base_code
-        self["expected_judgments"] = expected_judgments
-        self["payment_max_cents"] = payment_max_cents
-        self.__dict__ = self
-
-
-def get_job_config(con, job_id, table="jobs"):
-    """
-    Return a job config or the default configuration if the job wasn't found
-    :param con: (Connection)
-    :param job_id:
-    :param table:
-    """
-    _job_config = None
-    if table_exists(con, table):
-        with con:
-            _job_config = con.execute(f"SELECT * from {table} WHERE job_id==?", (job_id,)).fetchone()
-    if _job_config is None:
-        job_config = JobConfig(job_id, api_key='')
-    else:
-        job_config = JobConfig(**_job_config)
-    return job_config
 
 def get_treaments_infos(con, treatments):
     all_infos = {}
-    #treatments = ["t31", "t30"]
     for treatment in treatments:
         infos = {}
         table_survey = f"result__{treatment}_survey"
         if table_exists(con, table_survey):
-            # sql_completed_surveys = f"""
-            # SELECT
-            #     job_id,
-            #     case completion_code WHEN 'dropped' THEN
-            #         "dropped"
-            #     ELSE
-            #         "completed"
-            #     END dropped,
-            #     count(*) value
-            # FROM {table_survey}
-            # GROUP BY job_id, completion_code='dropped'
-            # """
-            sql_completed_surveys = f"""
-            SELECT
-                job_id,
-                case completion_code WHEN 'dropped' THEN
-                    "dropped"
-                ELSE
-                    "completed"
-                END,
-                count(*) value
-            FROM {table_survey}
-            GROUP BY job_id, completion_code='dropped'
-            """
-
             sql_completed_surveys = f"""select
                 r.job_id,
                 count(*) count,
@@ -99,20 +26,8 @@ def get_treaments_infos(con, treatments):
             """
 
             res = con.execute(sql_completed_surveys).fetchall()
-            app.logger.warning(f"RESULT_raw: {[list(item) for item in res]}")
             res = [{'job_id':item[0], 'count' : item[1], "dropped":item[2]} for item in res]
-            app.logger.warning(f"RESULT: {res}")
-
-            # sql_completed_surveys = f"""
-            # SELECT
-            #     job_id,
-            #     count(*)
-            # FROM {table_survey}
-            # GROUP BY job_id
-            # """
-
-            # completed_surveys_raw = dict(con.execute(sql_completed_surveys).fetchall())
-            infos["survey"] =res    #{str(itm):itm for itm in completed_surveys_raw}
+            infos["survey"] =res
         else:
             infos["survey"] = None
         
@@ -147,24 +62,6 @@ def get_treaments_infos(con, treatments):
         all_infos[treatment] = infos
     return all_infos
 
-
-def update_job(con, job_id, job_config, table="jobs"):
-    if not table_exists(con, table):
-        insert(pd.DataFrame(data=[job_config]), table=table, con=con)
-    else:
-        with con:
-            check = con.execute(f"SELECT job_id from jobs where job_id==?", (job_id,)).fetchone()
-            if check:
-                update(
-                    f"UPDATE {table} SET api_key=?, base_code=?, expected_judgments=?, payment_max_cents=?",
-                    args=(job_config['api_key'], job_config['base_code'], job_config['expected_judgments'], job_config['payment_max_cents']),
-                    con=con
-                )
-            else:
-                insert(pd.DataFrame(data=[job_config]), table=table, con=con)
-
-            
-    
 ####################
 
 
